d86f93a5396f756cdb93175d50c90bac254adb82
Kundan Kumar <kundan29304@gmail.com>
2026-08-07 10:47:07 +0530

    Time: 2 ms (20.18%) | Memory: 69.4 MB (23.60%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int sum = 0;
-        int minLength = Integer.MAX_VALUE;
+        int n = nums.length;
+        int  left = 0;
+        int  window = 0;
+        int ans = n+1;
 
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
+        for(int right = 0; right< n; right++){
+            window += nums[right]; //  huym window ko  store kar rahe hian 
 
-            while (sum >= target) {
-                minLength = Math.min(minLength, right - left + 1);
-                sum -= nums[left];
-                left++;
+            while(window >= target){
+                ans = Math.min(ans ,  right-left+1);
+                window = window-nums[left++]; //  pichce se ak  element  ko remove kar raha hain 
+
             }
         }
 
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        return ans == n+1 ? 0 : ans ;
     }
 }
