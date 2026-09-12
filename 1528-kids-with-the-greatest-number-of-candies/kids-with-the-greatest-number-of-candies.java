ebf5547552065161f1ee6c1debde933564808e24
Kundan Kumar <kundan29304@gmail.com>
2026-09-12 10:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (25.08%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -1,14 +1,12 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List <Boolean > result  =  new  ArrayList<Boolean>();
-
         int maxCandies = 0;
         for (int candi : candies) {
             if (candi > maxCandies) {   // yaha  hum  maxCandies nikalrahe hain 
                 maxCandies = candi;
             }
         }
-
           for(int i = 0 ; i < candies.length; i++){
             if( maxCandies<=candies[i] + extraCandies  ){
                 result.add(true);
@@ -20,6 +18,10 @@ class Solution {
           }
 
           return result;
+
+
+
+
         
     }
 }
